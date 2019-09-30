f55f00ae44889d53a3818b57ac1e5ade05d449a8
WWWW-coder <55945792+WWWW-coder@users.noreply.github.com>
2019-09-30 16:16:47 +0630

    Add files via upload

---
+++ christmas.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(){
+std::cout << "                                                                        _ /\ _                     \n";
+std::cout << " X   *     X      ''    M    *    M     *     A       *      S S S      \$  $/                     \n";
+std::cout << " X         X  *   ''    M M     M M   *    A     A         S  *    S    /_  _\                     \n";
+std::cout << "   X    X        o      M M     M M  *  A      *    A      S             $\ /$                     \n";
+std::cout << "     X   *          *   M    M    M     A  A  A  A  A     *  S S S        /$\                      \n";
+std::cout << "   X *  X      *        M   *     M *   A           A  *      *    S   * /$ $\                     \n";
+std::cout << " X         X      *     M    *    M  *  A     *     A      S       S   /$_   _$\    _()_           \n";
+std::cout << " X  *  *   X            M   *     M  *  A       *   A        S S S    * $ \_/ $    |____|          \n";
+std::cout << "  ................~~~~~~~~~___________________===================="""""""""""""""",,,,,,,,,,,,     \n";
+return 0;
+}
